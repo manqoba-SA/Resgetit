4f946148d32806b81ab4e616eaa4de2da2f672a0
Manqoba <nqobi.it4073@gmail.com>
2022-05-07 20:55:26 +0200

    first push

--- base/models.py
+++ base/models.py
@@ -74,7 +74,7 @@ class Product(models.Model):
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=250, null=True, blank=True)
     rating = models.IntegerField(null=True, blank=True, default=0)
     comment = models.TextField(null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)

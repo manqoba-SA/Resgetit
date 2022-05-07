a96f6feaeda3c70988de721a324f3162c44a0cb6
Manqoba <nqobi.it4073@gmail.com>
2022-05-07 12:18:48 +0200

    first push

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ] 
